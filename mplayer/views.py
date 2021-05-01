a1593532cd0bc4ff3962f40ee25622d5ec0e6164
Ishan <1999ishan.sharma@gmail.com>
2021-05-01 11:55:51 +0530

    Second commit

--- mplayer/views.py
+++ mplayer/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Song
+import json
 
 # Create your views here.
 def index(request):
-    return render(request, 'musicplayer.html')
+    songs = Song.objects.all()
+    Allsongs = []
+    for item in songs:
+        Allsongs.append({'songname': item.songname, 'artist': item.artist, 'img': item.img,  'audio': item.audio})
+        response = json.dumps(Allsongs, default=str)
+    return render(request, 'musicplayer.html', {'data': response, 'songs': songs})
